995018df7b9dd56d7870d25a2198543f4e264590
Nurlan Joldibaev <nurlanjoldibaev7@gmail.com>
2026-01-24 23:50:30 +0500

    feat: improve eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,14 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default tseslint.config(
   {
     ignores: [
       'eslint.config.mjs',
       'test/*',
+      'test-concurrency.ts',
       'src/generated/*',
       'prisma/seed.ts',
       'prisma/migrations/*',
@@ -18,12 +20,15 @@ export default tseslint.config(
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
   {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
+      sourceType: 'module',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
@@ -32,9 +37,21 @@ export default tseslint.config(
   },
   {
     rules: {
+      '@typescript-eslint/no-unused-vars': 'off', // Turn off base rule and use plugin version instead
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'error',
       'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
